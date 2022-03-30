8e52c0881693d30352b9ee61b36895441584ad95
Vaishnav Bharadwaj <vaishnavbharadwaj@Vaishnavs-MacBook-Air.local>
2022-03-30 12:31:52 +0100

    added user route file

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 require("dotenv").config();
 
+const users = require('./routes/api/users')
+
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.send("hemllo")
 })
 
+// routes use
+app.use('/api/users', users)
 
+//PORT in env file
 const PORT = process.env.PORT || 8080
 
 app.listen( PORT, () => console.log(`App running on localhost: ${PORT}`))
