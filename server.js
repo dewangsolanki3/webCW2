e830d9c848598df472902e8d320765af4bbc7dde
dewangsolanki3 <dewangsolanki3@gmail.com>
2022-04-05 22:17:03 +0100

    mongoose connection

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require('express')
-require("dotenv").config();
+require("dotenv").config()
+const mongoose = require('mongoose')
+
+let MONGODB_URI = "mongodb+srv://dewangsolanki3:Cluster@123@cluster0-n0bdv.mongodb.net/devconnect?retryWrites=true&w=majority"
+
+// Mongoose connection
+// mongoose.connect( MONGODB_URI || 'mongodb://localhost:27017/React-Galaxy', { useNewUrlParser: true , useUnifiedTopology: true } , error => {
+//     error ? console.log("=======Oops Dewang=======" , error) : console.log("======Dewang MongoDB Successful======")  
+// })
+
+
+// mongoose.connect(MONGODB_URI).then(() => console.log('MongoDb connected')).catch(err => console.log(err))
+
+
 
 const app = express()
 
@@ -11,3 +24,4 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 8080
 
 app.listen( PORT, () => console.log(`App running on localhost: ${PORT}`))
+
