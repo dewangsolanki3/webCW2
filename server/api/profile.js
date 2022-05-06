83e9cec576e4d936f6f1462328313179aab17e81
Vaishnav Bharadwaj <vaishnavbharadwaj@Vaishnavs-MacBook-Air.local>
2022-05-06 21:23:43 +0100

    tweeks

--- server/api/profile.js
+++ server/api/profile.js
@@ -81,42 +81,42 @@ router.get('/user/:user_id',(req, res) => {
 //create user profile
 router.post('/',passport.authenticate('jwt', {session: false}),(req,res) => {
 
-    const profileFields = {}
+    const profile_data = {}
     profileFields.user = req.user.id;
-    if(req.body.handle) profileFields.handle = req.body.handle
-    if(req.body.company) profileFields.company = req.body.company
-    if(req.body.website) profileFields.website = req.body.website
-    if(req.body.location) profileFields.location = req.body.location
-    if(req.body.bio) profileFields.bio = req.body.bio
-    if(req.body.status) profileFields.status = req.body.status
-    if(req.body.githubusername) profileFields.githubusername = req.body.githubusername
+    if(req.body.handle) profile_data.handle = req.body.handle
+    if(req.body.company) profile_data.company = req.body.company
+    if(req.body.website) profile_data.website = req.body.website
+    if(req.body.location) profile_data.location = req.body.location
+    if(req.body.bio) profile_data.bio = req.body.bio
+    if(req.body.status) profile_data.status = req.body.status
+    if(req.body.githubusername) profile_data.githubusername = req.body.githubusername
     
     // skills split with ,
     if(typeof req.body.skills !=='undefined'){
-        profileFields.skills = req.body.skills.split(',')
+        profile_data.skills = req.body.skills.split(',')
     }
 
     //social 
-    profileFields.social = {}
-    if(req.body.youtube) profileFields.social.youtube = req.body.youtube
-    if(req.body.twitter) profileFields.social.twitter = req.body.twitter
-    if(req.body.facebook) profileFields.social.facebook = req.body.facebook
-    if(req.body.linkedin) profileFields.social.linkedin = req.body.linkedin
-    if(req.body.instagram) profileFields.social.instagram = req.body.instagram
+    profile_data.social = {}
+    if(req.body.youtube) profile_data.social.youtube = req.body.youtube
+    if(req.body.twitter) profile_data.social.twitter = req.body.twitter
+    if(req.body.facebook) profile_data.social.facebook = req.body.facebook
+    if(req.body.linkedin) profile_data.social.linkedin = req.body.linkedin
+    if(req.body.instagram) profile_data.social.instagram = req.body.instagram
 
     Profile.findOne({ user: req.user.id })
     .then(profile => {
         if(profile){
-            Profile.findOneAndUpdate({ user: req.user.id }, { $set: profileFields }, {new: true})
+            Profile.findOneAndUpdate({ user: req.user.id }, { $set: profile_data }, {new: true})
             .then(profile => res.json(profile))
         } else {
-            Profile.findOne({handle: profileFields.handle}).then(profile => {
+            Profile.findOne({handle: profile_data.handle}).then(profile => {
                 if(profile){
                     errors.handle = "that handle already exists";
                     res.status(400).json(errors)
                 }
 
-                new Profile(profileFields).save().then(profile => res.json(profile))
+                new Profile(profile_data).save().then(profile => res.json(profile))
             })
         }
     })
